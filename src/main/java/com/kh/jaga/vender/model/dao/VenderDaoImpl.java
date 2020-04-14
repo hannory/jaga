08855def2c743042638c297a71742d605b21a603
user <user@DESKTOP-723RNEO>
2020-04-14 16:59:39 +0900

    레파지토리 수정

--- src/main/java/com/kh/jaga/vender/model/dao/VenderDaoImpl.java
+++ src/main/java/com/kh/jaga/vender/model/dao/VenderDaoImpl.java
@@ -1,15 +1,16 @@
 package com.kh.jaga.vender.model.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.kh.jaga.vender.model.vo.Vender;
-
+@Repository
 public class VenderDaoImpl implements VenderDao{
 
 	@Override
 	public int insertVender(SqlSessionTemplate sqlSession, Vender v) {
 
-		return sqlSession.insert("Company.insertVender",v);
+		return sqlSession.insert("Vender.insertVender",v);
 	}
 
 }
