75ed54ec4eb81b80eecc9a762444dfbf6925584e
HY S <HY S@LAPTOP-OITURFNO>
2020-04-16 09:23:19 +0900

    매입매출전표 공급대가입력시 공급가액과 부가세 자동계산됨 web.xml수정

--- src/main/java/com/kh/jaga/slip/model/dao/ReceiptionDao.java
+++ src/main/java/com/kh/jaga/slip/model/dao/ReceiptionDao.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 
+import com.kh.jaga.slip.model.exception.receiptionException;
 import com.kh.jaga.slip.model.vo.Vender;
 
 public interface ReceiptionDao {
 
-	List<Vender> selectVenderList(SqlSessionTemplate sqlSession);
+	List<Vender> selectVenderList(SqlSessionTemplate sqlSession) throws receiptionException;
 
 }
--- src/main/java/com/kh/jaga/slip/model/service/ReceiptionService.java
+++ src/main/java/com/kh/jaga/slip/model/service/ReceiptionService.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.kh.jaga.slip.model.exception.receiptionException;
 import com.kh.jaga.slip.model.vo.Vender;
 
 @Service
 public interface ReceiptionService {
-	List<Vender> selectVenderList();
+	List<Vender> selectVenderList() throws receiptionException 	;
 }
