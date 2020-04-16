bd95c1c5833555a36c35d55feae04cc7a04905fc
hannory <hannory@DESKTOP-L6L6CUI>
2020-04-16 09:23:20 +0900

    FinStmt의 Model + log4j2

--- src/main/java/com/kh/jaga/finStmt/controller/FinStmtController.java
+++ src/main/java/com/kh/jaga/finStmt/controller/FinStmtController.java
@@ -1,7 +1,13 @@
 package com.kh.jaga.finStmt.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.jaga.finStmt.model.service.FinStmtService;
+import com.kh.jaga.finStmt.model.vo.IncomeStmt;
 
 /**
  * @author hannory
@@ -10,6 +16,9 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class FinStmtController {
+	@Autowired
+	private FinStmtService fss;
+	
 	@GetMapping("compTrialBal.fs")
     public String showCompTrialBal() {
        return "compTrialBal";
@@ -34,4 +43,33 @@ public class FinStmtController {
 	public String showStmtOfCashFlow() {
 		return "stmtOfCashFlow";
 	}	
+	
+	@RequestMapping("insertIncomeStmt.fs")
+	public String insertIncomeStmt(IncomeStmt i, Model model) {
+		
+		System.out.println("incomeStmt : " + i);
+		
+		//System.out.println("result : " + fss.insertIncomeStmt(i));
+		
+		fss.insertIncomeStmt(i);
+		
+		return "redirect:index.jsp";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/kh/jaga/finStmt/model/service/FinStmtService.java
@@ -0,0 +1,8 @@
+package com.kh.jaga.finStmt.model.service;
+
+import com.kh.jaga.finStmt.model.vo.IncomeStmt;
+
+public interface FinStmtService {
+
+	int insertIncomeStmt(IncomeStmt i);
+}
