44777c6ffcd8de6b31cca2e71fbefb19139f7a82
hannory <hannory@DESKTOP-L6L6CUI>
2020-04-27 08:50:44 +0900

    로그인 체크 필터 추가

--- src/main/java/com/kh/jaga/common/CompanyLoginInterceptor.java
+++ src/main/java/com/kh/jaga/common/CompanyLoginInterceptor.java
@@ -10,30 +10,32 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class CompanyLoginInterceptor extends HandlerInterceptorAdapter {
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-
-		System.out.println("인터셉터 진입");
-		
-		return false;
-		//return super.preHandle(request, response, handler);
-	}
-	
 //	@Override
-//	public boolean preHandle(HttpServletRequest request,
-//			HttpServletResponse response, Object handler) throws IOException {
-//		
+//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+//			throws Exception {
+//
 //		System.out.println("인터셉터 진입");
 //		
-//		HttpSession session = request.getSession(false);
-//		if(session != null) {
-//			Object obj = session.getAttribute("loginCompany");
-//			if(obj != null)
-//				return true;
-//		}
-//		
-//		response.sendRedirect(request.getContextPath() + "/");
 //		return false;
+//		//return super.preHandle(request, response, handler);
 //	}
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws IOException {
+		
+		System.out.println("인터셉터 진입");
+		
+		HttpSession session = request.getSession(false);
+		if(session != null) {
+			Object obj = session.getAttribute("loginCompany");
+			if(obj != null)
+				return true;
+		}
+		
+		response.sendRedirect(request.getContextPath() + "/");
+		return false;
+		
+		
+	}
 }
